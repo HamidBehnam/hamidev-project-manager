e4f37cffbcf5d7b400d1a08acf29c277d87cf886
HamidBehnam <hamid.behnam@gmail.com>
2020-02-29 17:40:34 -0800

    add enums

--- functions/src/common/services/constants.service.ts
+++ functions/src/common/services/constants.service.ts
@@ -2,3 +2,23 @@ export enum ValidationDataSource {
     Headers,
     Body
 }
+
+export enum WorkPriority {
+    Urgent,
+    High,
+    Normal,
+    Low
+}
+
+export enum WorkStatus {
+    NotStarted,
+    Started,
+    Done,
+    Rejected,
+    Accepted
+}
+
+export enum ProjectMemberRole {
+    Manager,
+    Developer
+}
